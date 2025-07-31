78d4fce00b5f39da6538cd01c3f836a8fbb7956e
nitishjha18 <nitishjha1100@gmail.com>
2025-07-31 16:38:38 +0530

    Added RTE - Reat Time Editor in our project

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import logo from "./container/logo";
 import LogoutBttn from "./Header/LogoutBttn"
+import RTE from "./RTE"
 
 export {
     Header,
     Footer,
     Container,
     logo,
-    LogoutBttn
+    LogoutBttn,
+    RTE
 }
