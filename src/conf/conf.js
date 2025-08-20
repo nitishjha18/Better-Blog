e44e8bc98749ae4b144285ff375d0bd211816ad5
nitishjha18 <nitishjha1100@gmail.com>
2025-08-20 19:01:21 +0530

    changes in conf,js

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,4 +1,4 @@
-const config = {
+const conf = {
     appwriteURL: String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
